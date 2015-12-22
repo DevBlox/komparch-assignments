0dd3dfd9941d06926551b0465ff68ee68286ae06
DevBlox <l.praninskas@gmail.com>
2015-12-22 11:28:55 +0200

    swapping of sources works properly

--- STATSTXT/statstxt.asm
+++ STATSTXT/statstxt.asm
@@ -14,13 +14,16 @@ data_segment segment para public 'data'
                db 'Parameters:', 10, 13
                db '-i    ->   input text file', 10, 13
                db '-h    ->   print this help', 10, 13
-               db '-c    ->   console mode (default)', 10, 13, '$'
+               db '-c    ->   console mode', 10, 13, '$'
 
-  console_mode_message db 'Enter your text: $'
+  console_mode_message db 'Enter your text (up to 255 characters): $'
+  missing_file_param_message db 'Filename argument is missing!$'
+  open_file_failure db 'Failed to open file!$'
+  endline db 10, 13, '$'
 
+  ; ================== Filters ==================
   letter_a_desc db 'Amount of letters a$'
   letter_b_desc db 'Amount of letters b$'
-  endline db 10, 13, '$'
 
   filter struc
     func dw 0
@@ -34,11 +37,22 @@ data_segment segment para public 'data'
 
   number_filters dw 2
 
-  buffer_length equ 256
+  ; ================== Filters end ==================
+
+  ; ================ buffers begin =================
+  buffer_length equ 255
   buffer db buffer_length dup('$')
 
-  text_length equ 255
-  text db text_length dup('$')
+  text_buffer_length equ 255
+  text_buffer db text_buffer_length dup('$')
+  ; ================ buffers end =================
+
+  ; ================ buffer io handler data begin ===============
+  fill_buffer_proc_p dw 0
+  fetch_byte_proc_p dw 0
+  fetch_byte_offset dw 0
+  file_handle dw 0 ; unused for console
+  ; ================ buffer io handler data begin ===============
 
 data_segment ends
 
@@ -48,6 +62,78 @@ include ..\COMMON\utils.inc
 include ..\COMMON\stdlib.inc
 include ..\COMMON\comarg.inc
 
+; ================ buffer io handler procedures begin ===============
+read_byte proc
+    locals @@
+
+  @@start:
+    mov cx, fetch_byte_offset
+    cmp cx, text_buffer_length
+    jge @@refill
+
+    mov bx, fetch_byte_proc_p
+    call bx
+    jmp @@done
+
+  @@refill:
+    mov bx, fill_buffer_proc_p
+    call bx
+    cmp bx, -1
+    je @@no_more_data
+    jmp @@start
+
+  @@no_more_data:
+    mov al, -1
+  @@done:
+    ret
+read_byte endp
+; ================ buffer io handler procedures end =================
+; ================ data source specific begin ===========================
+console_fill proc
+    mov bx, -1
+    ret
+console_fill endp
+
+console_fetch_byte proc
+    locals @@
+    mov bx, fetch_byte_offset
+    mov al, text_buffer[bx]
+    cmp al, 0Dh
+    je @@return_no_char
+    inc fetch_byte_offset
+    ret
+
+  @@return_no_char:
+    mov al, -1
+    ret
+console_fetch_byte endp
+
+file_fill_buffer proc
+    locals @@
+    memset text_buffer, text_buffer_length, '$'
+    fread file_handle, text_buffer, text_buffer_length
+    mov bx, 0
+    mov fetch_byte_offset, 0
+    ret
+file_fill_buffer endp
+
+file_fetch_byte proc
+    locals @@
+    mov bx, fetch_byte_offset
+    mov al, text_buffer[bx]
+    cmp al, 0Ah
+    je @@close_file
+    inc fetch_byte_offset
+    jmp @@done
+
+  @@close_file:
+    fclose file_handle
+    mov al, -1
+  @@done:
+    ret
+file_fetch_byte endp
+; ================ data source specific end ===========================
+; ==================== parameter processing begin ====================
 print_help proc
     print help_message
     exit 0
@@ -56,14 +142,46 @@ print_help endp
 
 console_mode proc
     print console_mode_message
-    prepare_buffer text, text_length
-    stdin_buffer_read text
-    call process_text
+    prepare_buffer text_buffer, text_buffer_length
+    stdin_buffer_read text_buffer
+    print endline
+
+    mov fetch_byte_offset, 2
+
+    lea bx, console_fetch_byte
+    mov fetch_byte_proc_p, bx
+    lea bx, console_fill
+    mov fill_buffer_proc_p, bx
     ret
 console_mode endp
 
 file_mode proc
+    locals @@
+    memset buffer, buffer_length, 00h
+    call next_parameter_offset
+    cmp bx, -1
+    jle @@no_param_error_print
+    paramcpy buffer, buffer_length
+
+    lea bx, file_fill_buffer
+    mov fill_buffer_proc_p, bx
+
+    lea bx, file_fetch_byte
+    mov fetch_byte_proc_p, bx
+
+    fopen 00h, buffer
+    jc @@error_opening_file_print
+    mov file_handle, ax
+    call file_fill_buffer
     ret
+
+  @@no_param_error_print:
+    print missing_file_param_message
+    exit -1
+
+  @@error_opening_file_print:
+    print open_file_failure
+    exit -1
 file_mode endp
 
 process_parameters proc
@@ -73,14 +191,8 @@ process_parameters proc
     ret
 process_parameters endp
 
-process_text proc
-
-    ret
-process_text endp
-
-; ============================
-; filters begin
-; ============================
+; ==================== parameter processing end ====================
+; =================== filters procedures begin ===================
   letter_b_func proc
 
     ret
@@ -88,13 +200,11 @@ process_text endp
 
   letter_a_func proc
 
-
     ret
   letter_a_func endp
-; ============================
-; filters end
-; ============================
+; =================== filters procedures end ===================
 
+; =================== start of code execution ===================
   start:
     load_data_segment data_segment
 
@@ -103,7 +213,7 @@ process_text endp
     cmp bx, -2
     je exit_and_print_help
     cmp bx, -1
-    je exit_prog
+    je filter_stage
 
     paramcpy buffer, buffer_length
     call process_parameters
@@ -111,10 +221,19 @@ process_text endp
 
     jmp parameter_loop
 
-  exit_and_print_help:
-    print help_message
+  filter_stage:
+    call read_byte
+    cmp al, -1
+    je exit_prog
+
+    print_char al
+    jmp filter_stage
+
   exit_prog:
     exit 0
+  exit_and_print_help:
+    call print_help
+    ; ================== program end =========================
 
 code_segment ends
 end start
